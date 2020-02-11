47e6c734e0c5efdeeabab9f11c550f5c2dd4bb25
Stephen Rust <srust@blockbridge.com>
2020-02-11 12:46:03 -0500

    create gh-pages branch

--- build/commit.sh
+++ build/commit.sh
@@ -6,7 +6,7 @@ require_clean_work_tree "update static site"
 set -euo pipefail
 cbranch=$(git symbolic-ref --short HEAD)
 
-git checkout gh-pages
+git checkout -b gh-pages || true
 cp -r _site/* .
 git add .
 git diff --cached --quiet --exit-code || git commit -m "update static site"
